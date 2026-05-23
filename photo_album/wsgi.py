26b098472db4b27d38211fe7052789b0f125f10e
= <leolegitimas.official@gmail.com>
2026-05-23 20:31:06 +0800

    DEPLOY

--- photo_album/wsgi.py
+++ photo_album/wsgi.py
@@ -1,5 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_album.settings')
+
 application = get_wsgi_application()
+
+if os.environ.get('RUN_MIGRATIONS_ON_STARTUP', '1') == '1':
+    call_command('migrate', interactive=False, verbosity=1)
